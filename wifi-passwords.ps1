3195ec4423875c5f420dcf9647d2961aa1d43a4e
Irek-h <82705344+Irek-h@users.noreply.github.com>
2021-12-27 10:50:39 +0100

    add script

---
+++ wifi-passwords.ps1
@@ -0,0 +1,6 @@
+﻿$all_profiles = netsh wlan show profiles 
+$test = $all_profiles.Trim() | Select-String 'All User Profile' | ForEach-Object { $_ -replace '(?<=\S)\s+:\s+(?=\S)','=' } 
+$profiles = $test -creplace '[^\\]*=','' 
+$all = foreach($item in $profiles) {netsh wlan show profile name=$item key=clear } 
+$all | ConvertFrom-String | select p2, p5 | Select-String "SSID" 
+$all | ConvertFrom-String | select p3, p5 | Select-String "Content"
